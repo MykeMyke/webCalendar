e2092ca9639160697dd9056b7fda5549856f9ac3
MykeMyke <mykemyke7356@gmail.com>
2025-02-07 10:11:42 +0000

    Remove old ResDM Bios

--- src/data/resDMbios.js
+++ src/data/resDMbios.js
@@ -1,8 +1,7 @@
 export const resDMbios = [
   {
     name: "Clark",
-    superpowers:
-      "Reflavoring and reskinning things to develop character concepts",
+    superpowers: "Reflavoring and reskinning things to develop character concepts",
     favMonster: "Illithilich",
     favMoment: "",
     otherInterests:
@@ -16,8 +15,7 @@ export const resDMbios = [
   },
   {
     name: "MykeMyke",
-    superpowers:
-      "Refined Roll20 automation and slick Theatre of the Mind sequences",
+    superpowers: "Refined Roll20 automation and slick Theatre of the Mind sequences",
     favMonster: "Gloom Weaver",
     favMoment:
       "A high level melee character was beating up on a Kraken. So rather than deal with the threat, the Kraken opted to throw them out to sea whenever they came close enough... Everybody laughs. Repeat!",
@@ -31,13 +29,11 @@ export const resDMbios = [
   },
   {
     name: "Liam James ",
-    superpowers:
-      "Running seven campaigns and a weekly one shot at the same time. ",
+    superpowers: "Running seven campaigns and a weekly one shot at the same time. ",
     favMonster: "Count Strahd von Zarovich.",
     favMoment:
       "Counterspelling the cleric's healing magic and then eating the fighter's brain. She had been losing her faith while in Maladomini, but in that darkest moment she called on her god for divine intervention and he answered her prayers.",
-    otherInterests:
-      "Book blogger and reviewer. I also run DnD sessions with authors and publishing professionals. ",
+    otherInterests: "Book blogger and reviewer. I also run DnD sessions with authors and publishing professionals. ",
     link1name: "My Link Tree",
     link2name: "",
     link1: "https://linktr.ee/wormholesandbookworms",
@@ -46,27 +42,12 @@ export const resDMbios = [
     image: "/img/ResDMs/Liam-James.png",
   },
   {
-    name: "Griffmeister",
-    superpowers: "Nat 20 death saves.  ",
-    favMonster: "Griffon or Hippogriff of course",
-    favMoment:
-      "Running a group of old uni mate dnd noobs through Rise of Tiamat during lockdown and creating 6 new dnd addicts in the process.",
-    otherInterests: "Rugby, lash, board games, video games and family.",
-    link1name: "Game Therapy",
-    link2name: "",
-    link1: "https://www.game-therapy.co.uk",
-    link2: "",
-    location: "London, England",
-    image: "/img/ResDMs/Griffmeister.png",
-  },
-  {
     name: "J",
     superpowers: "Earnest voice acting and curated adventure soundtracks",
     favMonster: "Hag",
     favMoment:
       "Having a vampire successfully charm a sorcerer who then cast Slow on her unwitting party. The absolutely ridiculous struggle they faced trying to kill the vampire in the darkness - whilst thusly sabotaged - was hilarious, but alarming too!",
-    otherInterests:
-      "Reading, art, gaming and collecting Ball-Jointed Dolls (that I paint by commission).",
+    otherInterests: "Reading, art, gaming and collecting Ball-Jointed Dolls (that I paint by commission).",
     link1name: "My Adventure on DMsGuild",
     link2name: "Flickr",
     link1: "https://www.dmsguild.com/product/393912/WBWDCMOM01-One-Moment",
@@ -76,34 +57,17 @@ export const resDMbios = [
   },
   {
     name: "Corgibutt",
-    superpowers:
-      "Jumping from one server to another and keeping track of the games I'm running or playing in each of them",
+    superpowers: "Jumping from one server to another and keeping track of the games I'm running or playing in each of them",
     favMonster: "Bodaks",
     favMoment:
       "Hearing my players' voices cracking over voice chat because they're getting emotional and invested in what's going on in the adventure. Then I see them creating new characters or changing their current ones' alignment/class/subclass because of the story.",
-    otherInterests:
-      "Editing character VTT tokens (by commission), Singing bad karaoke, Influencing newbies to try D&D",
+    otherInterests: "Editing character VTT tokens (by commission), Singing bad karaoke, Influencing newbies to try D&D",
     link1name: "My Ko-fi",
     link2name: "Leave me a Review",
     link1: "https://bit.ly/FeedTheCorgi",
     link2: "https://bit.ly/ReviewTheCorgi",
     location: "Manila, Philippines",
     image: "/img/ResDMs/Corgibutt.png",
-  },
-  {
-    name: "Shane H",
-    superpowers: "I'm just making it all up as I go along",
-    favMonster: "Star Spawn Mangler",
-    favMoment:
-      "Having a Paladin player tell me, after dealing with an Archfey I was running, that the Fey broke their character and tricked them into breaking their oath and going against everything they believed in. All through roleplay only, with no rolls involved. ",
-    otherInterests:
-      "Motorbikes, reading, Vtt map making, live music, gym, video games, travelling. ",
-    link1name: "",
-    link2name: "",
-    link1: "",
-    link2: "",
-    location: "Dublin, Ireland",
-    image: "/img/ResDMs/Shane-H.png",
   },
   {
     name: "Kuta",
@@ -119,20 +83,6 @@ export const resDMbios = [
     image: "/img/ResDMs/Placeholder.png",
   },
   {
-    name: "Daran",
-    superpowers: "Fostering party chicanery",
-    favMonster: "Pixie",
-    favMoment:
-      "A party befriending a spectator to the point of throwing it a birthday party, unaware that it's the target of their next quest. As players slowly figured out what they were being asked to do, one by one they blurted out: Ohhh noooooo!",
-    otherInterests: "Biking, hiking, travel, science",
-    link1name: "",
-    link2name: "",
-    link1: "",
-    link2: "",
-    location: "Chicago, IL, USA",
-    image: "/img/ResDMs/Daran.png",
-  },
-  {
     name: "Lex",
     superpowers: "Adapting on the fly. And setting the mood",
     favMonster: "Remorhaz",
@@ -142,24 +92,9 @@ export const resDMbios = [
       "Obsessed with travel. And Japanese history. When they collide, things get fun. Punk lifer with a huge vinyl collection.",
     link1name: "Some adventures I wrote on DM's Guild",
     link2name: "",
-    link1:
-      "https://www.dmsguild.com/browse.php?author=Lex%20Winter?affiliate_id=2235164",
+    link1: "https://www.dmsguild.com/browse.php?author=Lex%20Winter?affiliate_id=2235164",
     link2: "",
     location: "Baltimore, MD, USA",
     image: "/img/ResDMs/Lex.png",
-  },
-  {
-    name: "Malte",
-    superpowers: "Making sure a good time is had by all",
-    favMonster: "You'll find out...",
-    favMoment: "",
-    otherInterests:
-      "Swing music, Balboa dancing, HomeOffice while travelling, German Spitz, writing code to make my and others' life easy.",
-    link1name: "",
-    link2name: "",
-    link1: "",
-    link2: "",
-    location: "Hamburg, Germany",
-    image: "/img/ResDMs/Placeholder.png",
   },
 ];
